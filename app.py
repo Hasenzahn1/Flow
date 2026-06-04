354959f662e030835193947f70219a3c815f9622
Hasenzahn1 <motzer10@gmx.de>
2026-06-04 22:12:25 +0200

    Initial Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    socketio.run(app, host="0.0.0.0", port=5000)
