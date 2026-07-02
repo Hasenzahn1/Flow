110d3585a50f4bfe3fd03f5d852adfeb4bb04e37
Hasenzahn1 <75828222+Hasenzahn1@users.noreply.github.com>
2026-07-02 16:33:06 +0200

    Aktualisieren von app.py

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ operation_overview.register(app, socketio)
 
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port=5000)
+    socketio.run(app, host="0.0.0.0", port=5000, allow_unsafe_werkzeug=True)
